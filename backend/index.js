d22620f12418264e2fc98a90da76284cf934dc5d
Isolumi <hubertx98@gmail.com>
2025-04-02 20:48:55 -0400

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require('cors');
 
 const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('', routes);
 
